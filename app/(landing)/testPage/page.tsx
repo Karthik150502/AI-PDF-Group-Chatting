a92f55e7bc2d40be3a78af87709851da71d13cfb
Karthik150502 <karthikrdycool@gmail.com>
2024-10-08 16:34:25 +0530

    Latest

--- app/(landing)/testPage/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Signin from '@/components/cms_signin'
-export default function page() {
-    return (
-        <div>
-            <Signin />
-        </div>
-    )
-}
